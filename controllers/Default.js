99a2f605c3bb0768eef18accbcc037e2a57470dc
spyrchat <spyrchat@ece.auth.gr>
2025-02-02 20:22:56 +0200

    docs: add detailed JSDoc comments for author, book, and category endpoints

--- controllers/Default.js
+++ controllers/Default.js
@@ -1,8 +1,19 @@
 'use strict';
 
+// Importing utilities for writing JSON responses
 var utils = require('../utils/writer.js');
+
+// Importing the Default service that contains business logic
 var Default = require('../service/DefaultService');
 
+//
+// AUTHOR-RELATED ENDPOINTS
+//
+
+/**
+ * Handles DELETE request to delete an author by ID.
+ * Calls `Default.authorsAuthorIdDELETE` and returns the response.
+ */
 module.exports.authorsAuthorIdDELETE = function authorsAuthorIdDELETE (_, res, __, authorId) {
   Default.authorsAuthorIdDELETE(authorId)
     .then(function (response) {
@@ -13,6 +24,9 @@ module.exports.authorsAuthorIdDELETE = function authorsAuthorIdDELETE (_, res, _
     });
 };
 
+/**
+ * Handles GET request to fetch an author's details by ID.
+ */
 module.exports.authorsAuthorIdGET = function authorsAuthorIdGET (_, res, __, authorId) {
   Default.authorsAuthorIdGET(authorId)
     .then(function (response) {
@@ -23,6 +37,10 @@ module.exports.authorsAuthorIdGET = function authorsAuthorIdGET (_, res, __, aut
     });
 };
 
+/**
+ * Handles PUT request to update an author's details by ID.
+ * Expects a request body containing the new author details.
+ */
 module.exports.authorsAuthorIdPUT = function authorsAuthorIdPUT (_, res, __, body, authorId) {
   Default.authorsAuthorIdPUT(body, authorId)
     .then(function (response) {
@@ -33,6 +51,9 @@ module.exports.authorsAuthorIdPUT = function authorsAuthorIdPUT (_, res, __, bod
     });
 };
 
+/**
+ * Handles GET request to retrieve a list of all authors.
+ */
 module.exports.authorsGET = function authorsGET (_, res, __) {
   Default.authorsGET()
     .then(function (response) {
@@ -43,6 +64,10 @@ module.exports.authorsGET = function authorsGET (_, res, __) {
     });
 };
 
+/**
+ * Handles POST request to create a new author.
+ * Expects a request body containing author details.
+ */
 module.exports.authorsPOST = function authorsPOST (_, res, __, body) {
   Default.authorsPOST(body)
     .then(function (response) {
@@ -53,6 +78,13 @@ module.exports.authorsPOST = function authorsPOST (_, res, __, body) {
     });
 };
 
+//
+// BOOK-RELATED ENDPOINTS
+//
+
+/**
+ * Handles DELETE request to remove a book by ID.
+ */
 module.exports.booksBookIdDELETE = function booksBookIdDELETE (_, res, __, bookId) {
   Default.booksBookIdDELETE(bookId)
     .then(function (response) {
@@ -63,6 +95,9 @@ module.exports.booksBookIdDELETE = function booksBookIdDELETE (_, res, __, bookI
     });
 };
 
+/**
+ * Handles GET request to fetch a book's details by ID.
+ */
 module.exports.booksBookIdGET = function booksBookIdGET (_, res, __, bookId) {
   Default.booksBookIdGET(bookId)
     .then(function (response) {
@@ -73,6 +108,10 @@ module.exports.booksBookIdGET = function booksBookIdGET (_, res, __, bookId) {
     });
 };
 
+/**
+ * Handles PUT request to update a book's details by ID.
+ * Expects a request body containing new book details.
+ */
 module.exports.booksBookIdPUT = function booksBookIdPUT (_, res, __, body, bookId) {
   Default.booksBookIdPUT(body, bookId)
     .then(function (response) {
@@ -83,6 +122,9 @@ module.exports.booksBookIdPUT = function booksBookIdPUT (_, res, __, body, bookI
     });
 };
 
+/**
+ * Handles GET request to retrieve a list of all books.
+ */
 module.exports.booksGET = function booksGET (_, res, __) {
   Default.booksGET()
     .then(function (response) {
@@ -93,6 +135,10 @@ module.exports.booksGET = function booksGET (_, res, __) {
     });
 };
 
+/**
+ * Handles POST request to create a new book.
+ * Expects a request body containing book details.
+ */
 module.exports.booksPOST = function booksPOST (_, res, __, body) {
   Default.booksPOST(body)
     .then(function (response) {
@@ -103,6 +149,13 @@ module.exports.booksPOST = function booksPOST (_, res, __, body) {
     });
 };
 
+//
+// CATEGORY-RELATED ENDPOINTS
+//
+
+/**
+ * Handles DELETE request to remove a category by ID.
+ */
 module.exports.categoriesCategoryIdDELETE = function categoriesCategoryIdDELETE (_, res, __, categoryId) {
   Default.categoriesCategoryIdDELETE(categoryId)
     .then(function (response) {
@@ -113,6 +166,9 @@ module.exports.categoriesCategoryIdDELETE = function categoriesCategoryIdDELETE 
     });
 };
 
+/**
+ * Handles GET request to fetch a category's details by ID.
+ */
 module.exports.categoriesCategoryIdGET = function categoriesCategoryIdGET (_, res, __, categoryId) {
   Default.categoriesCategoryIdGET(categoryId)
     .then(function (response) {
@@ -123,6 +179,10 @@ module.exports.categoriesCategoryIdGET = function categoriesCategoryIdGET (_, re
     });
 };
 
+/**
+ * Handles PUT request to update a category's details by ID.
+ * Expects a request body containing the new category details.
+ */
 module.exports.categoriesCategoryIdPUT = function categoriesCategoryIdPUT (_, res, __, body, categoryId) {
   Default.categoriesCategoryIdPUT(body, categoryId)
     .then(function (response) {
@@ -133,6 +193,9 @@ module.exports.categoriesCategoryIdPUT = function categoriesCategoryIdPUT (_, re
     });
 };
 
+/**
+ * Handles GET request to retrieve a list of all categories.
+ */
 module.exports.categoriesGET = function categoriesGET (_, res, __) {
   Default.categoriesGET()
     .then(function (response) {
@@ -143,6 +206,10 @@ module.exports.categoriesGET = function categoriesGET (_, res, __) {
     });
 };
 
+/**
+ * Handles POST request to create a new category.
+ * Expects a request body containing category details.
+ */
 module.exports.categoriesPOST = function categoriesPOST (_, res, __, body) {
   Default.categoriesPOST(body)
     .then(function (response) {
